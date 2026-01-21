2ab6daa5fad1980914eae02384d8a2a089862614
sandwh1ched <sandwh1ched.real@gmail.com>
2026-01-21 07:05:33 -0600

    Added new assignment support

---
+++ apps/thumbtack/experimental/js/newEditDialog.js
@@ -0,0 +1,29 @@
+const assignmentForm = document.getElementById("newEditForm");
+
+assignmentForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = new FormData(assignmentForm);
+    
+    const assignment = new Assignment(
+        data.get("name"),
+        data.get("course"),
+        data.get("progress"),
+        data.get("worth"),
+        data.get("deadline")
+    );
+    assignments.push(assignment);
+
+    synchronizeAndRender();
+
+    assignmentForm.reset();
+    document.getElementById("newEditDialog").close();
+});
+
+function showNewEditDialog() {
+    document.getElementById("newEditDialog").showModal();
+}
+
+function closeNewEditDialog() {
+    document.getElementById("newEditDialog").close();
+}
